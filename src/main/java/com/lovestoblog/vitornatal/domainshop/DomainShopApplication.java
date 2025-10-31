653d15f71f89d58cd68c820bc63e8304e825af99
Vitor Natal <vitornc31@gmail.com>
2025-10-31 18:08:56 -0300

    first commit

---
+++ src/main/java/com/lovestoblog/vitornatal/domainshop/DomainShopApplication.java
@@ -0,0 +1,13 @@
+package com.lovestoblog.vitornatal.domainshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lovestoblog/vitornatal/domainshop/DomainShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovestoblog.vitornatal.domainshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
