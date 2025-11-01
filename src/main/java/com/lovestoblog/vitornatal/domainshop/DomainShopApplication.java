87fe5dc79ca668797ba4cf8d675a108a6c5397ce
Vitor Natal <vitornc31@gmail.com>
2025-10-31 22:57:30 -0300

    fix: adds the @EnableFeignClients annotation

--- src/main/java/com/lovestoblog/vitornatal/domainshop/DomainShopApplication.java
+++ src/main/java/com/lovestoblog/vitornatal/domainshop/DomainShopApplication.java
@@ -2,8 +2,10 @@ package com.lovestoblog.vitornatal.domainshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DomainShopApplication {
 
     public static void main(String[] args) {
