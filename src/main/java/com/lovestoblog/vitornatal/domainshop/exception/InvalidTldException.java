f5d7abeedb9f0d7648e919f5b025564ea4b74e57
Vitor Natal <vitornc31@gmail.com>
2025-11-02 14:26:17 -0300

    feat: adds the InvalidTldException

---
+++ src/main/java/com/lovestoblog/vitornatal/domainshop/exception/InvalidTldException.java
@@ -0,0 +1,7 @@
+package com.lovestoblog.vitornatal.domainshop.exception;
+
+public class InvalidTldException extends RuntimeException{
+    public InvalidTldException(String message){
+        super(message);
+    }
+}
